e5b7a4790835a8d99dc71fb792f7a824d6c0e60a
Forvisk <adrianojr11@gmail.com>
2019-03-29 12:40:38 -0300

    making the ant walk...or trying at least

--- T1 - Dead Ant Ant Colector/classes/ant.py
+++ T1 - Dead Ant Ant Colector/classes/ant.py
@@ -1,10 +1,41 @@
 from random import randint
+import threading
+import time
 
-class Ant:
+exitFlag = 0
+
+class Ant (threading.Thread):
 	code = 0
-	pos = [-0, -0]
+	pos = [ 0, 0]
+	nextmove = [ 1, 1]
+	carry = False
 	
-	def __init__(self, code, pos):
-		self.code = code
-		self.carry = False
+	def __init__(self, threadID, pos):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
 		self.pos = pos
+
+	def run(self):
+		print( 'Iniciando ', self.threadID)
+		#print_time( self.threadID, 3, 1)
+		self.move()
+		print( 'finalizando ', self.threadID)
+
+	def move( self):
+		#lock ambiente
+		ambiente[ self.pos[0]][ self.pos[1]] -= 2
+		self.pos[0] += self.nextmove[0]
+		self.pos[1] += self.nextmove[1]
+		print( self.pos)
+		ambiente[ self.pos[0]][ self.pos[1]] += 2
+		#unlockambiente
+
+
+
+def print_time( threadID, counter, delay):
+		while counter:
+			if exitFlag:
+				threadName.exit()
+			time.sleep(delay)
+			print( threadID, ' - ', time.ctime( time.time()))
+			counter -= 1
--- T1 - Dead Ant Ant Colector/main.py
+++ T1 - Dead Ant Ant Colector/main.py
@@ -1,14 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from random import randint
-from classes.ant import *
-
+import threading
+import time
+#
 n = 10
 m = 10
 nItens = 15
 nFormigas = 5
-ambiente = 0
+ambiente = []
 agentes = []
+tempoExecucao = 0
+tempoMaximo = 2
 
 ## Matriz
 ## vazio = 0
@@ -17,11 +20,17 @@ agentes = []
 ## formiga e item = 3
 
 def main( ):
-	print(ambiente)
-	inicializaItens()
-	inicializaFormigas()
+	#print(ambiente)
+	inicializa()
 
-def inicializaItens( ):
+	tempoExecucao = 0
+	while tempoExecucao < tempoMaximo:
+		tempoExecucao += 1
+		time.sleep(15)
+		print(ambiente)
+
+
+def inicializa():
 	print('Iniciando os ', nItens, ' Itens')
 	if (nItens < 1):
 		return
@@ -35,8 +44,6 @@ def inicializaItens( ):
 			i += 1
 		if( i >= nItens):
 			break
-
-def inicializaFormigas():
 	print('Inicializando ', nFormigas, ' Formigas')
 	if (nFormigas < 1):
 		return
@@ -51,8 +58,9 @@ def inicializaFormigas():
 			i += 1
 		if( i >= nFormigas):
 			break
-
-
+	print(ambiente)
+	for i in range( 0, len(agentes)):
+		agentes[i].start()
 
 def contaItens():
 	contItens = 0
@@ -62,15 +70,76 @@ def contaItens():
 			if ( ambiente[i][j] == 1):
 				contItens += 1
 			else:
-				if ( ambiente[i][j] == 2):
+				if ( ambiente[i][j] > 2):
 					contFormigas += 1
 	print(contItens, ' Itens\n', contFormigas, ' formigas')
 	return [contItens, contItens]
+
+#### Classe Ant
+#
+class Ant (threading.Thread):
+	code = 0
+	posX = 0
+	posY = 0
+	nextmove = [ 1, 1]
+	carry = False
+	
+	def __init__(self, threadID, pos):
+		threading.Thread.__init__(self)
+		self.threadID = threadID
+		self.posX = pos[0]
+		self.posY = pos[1]
+
+	def run(self):
+		print( 'Iniciando formiga ', self.threadID, ' em posicao [', self.posX, ',', self.posY, ']')
+		#print_time( self.threadID, 3, 1)
+		self.decide()
+		self.move()
+		print( 'Finalizando ', self.threadID)
+
+	#Funções decisoras
+	# eixoX 1 = direita, -1 = esquerda
+	# eixoY 1 - baixo, -1 = cima
+
+	def decide( self):
+		contL = contI = 0
+		if (ambiente[ self.posX][self.posY] == 3):	# Item na posição da formiga
+			pass
+		else:	# Movimento
+			Lx = Ly = 1
+			lx = ly = -1
+			if self.posX+1 >= n:
+				Lx = 0
+			if self.posX-1 < 0:
+				lx = 0
+			if self.posY+1 >= m:
+				Ly = 0
+			if self.posY-1 < 0:
+				ly = 0
+			print( self.threadID, ' - ', lx, Lx, ly, Ly)
+
+			for x in range( lx, Lx):
+				for y in range( ly, Ly):
+					if ( x != 0 and y != 0):
+						if ()
+			pass
+		pass
+
+	def move( self):
+		#lock ambiente
+		ambiente[ self.posX][ self.posY] -= 2
+		self.posX += self.nextmove[0]
+		self.posX += self.nextmove[1]
+		print( '[', self.posX, ',', self.posY, ']')
+		ambiente[ self.posX][ self.posY] += 2
+		#unlockambiente
+
+#
+#### Classe Ant
 
 print('Iniciando')
 size = [n,m]
 ambiente = np.zeros( size)
 main()
-print(ambiente)
 contaItens()
 print('Finalizando')
