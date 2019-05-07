ec323e5a13d9cbdb81b6e3518d0e1e3912de4099
Forvisk <adrianojr11@gmail.com>
2019-05-07 18:25:51 -0300

    SA finalizado - ficou uma bosta

--- T2 - Simulated Anneling - 3-sat/search.py
+++ T2 - Simulated Anneling - 3-sat/search.py
@@ -1,5 +1,6 @@
 # xor == ^
-import random as rd
+from random import randint, seed
+from math import exp
 
 NVEZES = 10
 nVariaveis = 0
@@ -28,7 +29,7 @@ def leituraArquivo(file):
 
 	l = f.readline()
 	toGetSize = l.split()
-	print( toGetSize)
+	#print( toGetSize)
 
 	nVariaveis = int(toGetSize[1])
 	nClausulas = int(toGetSize[2])
@@ -50,14 +51,10 @@ def leituraArquivo(file):
 		
 	f.close()
 
-def randomSearch(varList):
+def randomSearch( varList):
+	global nClausulas
 	bestSets = []
 	bestNVerdades = 0
-
-	global nVariaveis
-	global nClausulas
-	global listClaVar
-	global listClaNot
 
 	nVerdades = avalia( varList)
 	#print("Set:", varList, "Resultados Verdadeiros:",nVerdades)
@@ -68,12 +65,72 @@ def randomSearch(varList):
 	elif nVerdades == bestNVerdades:
 		bestSets.append(varList)
 
-	print("Clausulas Verdadeiras:", bestNVerdades)
-	for lBest in bestSets:
-		print("Set:", lBest)
+	print("RS - Clausulas Verdadeiras:", bestNVerdades, "de", nClausulas)
+	#for lBest in bestSets:
+	#	print("Set:", lBest)
 
-def SASearch(varList):
-	pass
+def geraTinicial( varList):
+	Inicial = avalia(varList)
+	nCustosV = []
+	k=0
+	while k <= 0:
+		for i in range(10):
+			vizinho = geraVizinho(varList)
+			nCustosV.append(Inicial - avalia(vizinho))
+		k = max(nCustosV)
+	#gera temperatura aqui
+	#print(k)
+	return k
+
+def SASearch( varList):
+	global nClausulas
+	SAmax = 10
+	nk = 0
+	T = Tinicial = geraTinicial( varList)
+	cvvarList = bestNVerdades = avalia( varList)
+	bestSet = varList
+	print("SA- bestSet inicial",bestNVerdades, "Tinicial",Tinicial)
+	taxa = 0.9
+	while T > 0.001:
+		itT = 0
+		while itT < SAmax:
+			nk += 1
+			itT += 1
+			vizinho = geraVizinho(varList)
+			cvVizinhos = avalia(vizinho)
+
+			delta = cvVizinhos - cvvarList
+			#print("SA-",cvVizinhos,cvvarList)
+			if delta > 0:#ideia e maximizar
+				varList = vizinho
+				cvvarList = cvVizinhos
+				#print("SA- Novo varList", cvvarList)
+				if cvVizinhos > bestNVerdades:
+					#print("SA- Novo bestSet", cvVizinhos, ">", bestNVerdades)
+					bestSet = vizinho
+					bestNVerdades = cvVizinhos
+
+			else:
+				r = randint(0,100)/100
+				ex = delta/T
+				#print("SA: -r=",-r,";",delta,"/",T,"=",ex)
+				if -r >= exp(ex):
+					#print("SA",r,"<",exp(ex))
+					varList = vizinho
+					cvvarList = cvVizinhos
+					#print("SA- Novo varList", cvvarList)
+		T = round(T * taxa,10)
+		#print("SA- temperatura:",T)
+
+	print("SA - Clausulas Verdadeiras:", bestNVerdades, "de", nClausulas)
+	#print("Set:", bestSet)
+
+
+def geraVizinho( varList):
+	global nVariaveis
+	bTroca = randint(0,nVariaveis-1)
+	varList[bTroca] = not varList[bTroca]
+	return varList
 
 def avalia( varList):
 	global nClausulas
@@ -93,7 +150,7 @@ def geraRandomSet():
 	varList = []
 	for i in range(0, nVariaveis):
 		choice = True
-		if rd.randint(0,1) == 0:
+		if randint(0,1) == 0:
 			choice = False
 		varList.append(choice)
 	return varList
@@ -105,14 +162,17 @@ def endTimer(seq):
 	pass
 
 def main(file):
+	print(file)
 	leituraArquivo(file)
-	listasIniciais = []
-	for i in range(NVEZES):
-		listasIniciais.append(geraRandomSet())
+	listasIniciais = geraRandomSet()
 
 	for i in range(NVEZES):
+		#print("Estado Inicial",i,":",listasIniciais)
 		iniTimer(i)
-		randomSearch(listasIniciais[i])
+		randomSearch(listasIniciais)
+		endTimer(i)
+		iniTimer(i)
+		SASearch(listasIniciais)
 		endTimer(i)
 
 
